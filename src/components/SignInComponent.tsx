3284000440c3729f7616d32da5931e0caa3b5d29
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-02 14:15:30 +0530

    fixed cors issue && added admin login functionality

--- src/components/SignInComponent.tsx
+++ src/components/SignInComponent.tsx
@@ -1,6 +1,40 @@
 import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignInAuthComponent(){
+    const navigate= useNavigate();
+    const [email ,setEmail]=useState<string>("");
+    const [password, setPassword]=useState<string>("");
+
+    async function signin() {
+        if(!email || !password){
+            throw new Error("please enter credentials");
+            return;
+        }
+
+        try{
+            const response= await axios.post(`${import.meta.env.VITE_API_URL}/admin/signin`,{
+                email:email,
+                password:password
+            })
+            console.log("response of admin signin- ", response);
+
+            
+
+            if(response.status==200){
+                localStorage.setItem("admin", JSON.stringify(response.data.admin));
+                localStorage.setItem("token", response.data.token);
+                navigate("/home")
+                console.log("admin loggedin successfully");
+            }
+
+        }catch(e){
+            console.log("error in signinButton", e);
+            throw e;
+        }
+    }
 
     return(
         <Box
@@ -19,10 +53,29 @@ export default function SignInAuthComponent(){
                         gap: 2,
                     }}
                     >
-                        <Typography variant="h4">SignIn</Typography>
-                        <TextField id="" label="Email" variant="outlined"/>
-                        <TextField id="" label="Password" variant="outlined"/>
-                        <Button variant="contained">Sign In</Button>
+                        <Typography  variant="h4">SignIn</Typography>
+                        <TextField 
+                            onChange={
+                                (e:React.ChangeEvent<HTMLInputElement>)=>setEmail(e.target.value)
+                            }
+                            id="" 
+                            label="Email" 
+                            variant="outlined"
+                        />
+                        <TextField 
+                            onChange={
+                                (e:React.ChangeEvent<HTMLInputElement>)=>setPassword(e.target.value)
+                            }
+                            id="" 
+                            label="Password" 
+                            variant="outlined"
+                        />
+                        <Button
+                            onClick={signin} 
+                            variant="contained"
+                        >   
+                            Sign In
+                        </Button>
                     </Paper>    
                 </Container>
         </Box>
