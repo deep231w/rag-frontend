ab3c76ed09209441d2e67e916bf64f2059c9bb59
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-02 14:22:08 +0530

    added signup func

--- src/components/SignUpAuthComponent.tsx
+++ src/components/SignUpAuthComponent.tsx
@@ -1,6 +1,41 @@
 import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AuthComponent(){
+    const navigate= useNavigate();
+    const [email ,setEmail]=useState<string>("");
+    const [password, setPassword]=useState<string>("");
+    const [name, setName]=useState<string>("");
+
+
+    async function signup() {
+        if(!email || !password || !name){
+            throw new Error("please enter credentials");
+            return;
+        }
+
+        try{
+            const response= await axios.post(`${import.meta.env.VITE_API_URL}/admin/signup`,{
+                name:name,
+                email:email,
+                password:password
+            })
+            console.log("response of admin signin- ", response);
+
+            if(response.status==200){
+                localStorage.setItem("admin", JSON.stringify(response.data.admin));
+                localStorage.setItem("token", response.data.token);
+                navigate("/home")
+                console.log("admin loggedin successfully");
+            }
+
+        }catch(e){
+            console.log("error in signinButton", e);
+            throw e;
+        }
+    }
 
     return(
         <Box
@@ -20,10 +55,31 @@ export default function AuthComponent(){
                     }}
                     >
                         <Typography variant="h4">SignUp</Typography>
-                        <TextField id="" label="Name" variant="outlined"/>
-                        <TextField id="" label="Email" variant="outlined"/>
-                        <TextField id="" label="Password" variant="outlined"/>
-                        <Button variant="contained">Sign Up</Button>
+                        <TextField 
+                            onChange={(e)=>setName(e.target.value)}
+                            id="" 
+                            label="Name" 
+                            variant="outlined"
+                        />
+                        <TextField 
+                            onChange={(e)=>setEmail(e.target.value)}
+                            id="" 
+                            label="Email" 
+                            variant="outlined"
+                        />
+                        <TextField 
+                            onChange={(e)=>setPassword(e.target.value)}
+                            id="" 
+                            label="Password" 
+                            variant="outlined"
+                        />
+
+                        <Button
+                            onClick={signup} 
+                            variant="contained"
+                        >
+                            Sign Up
+                        </Button>
                     </Paper>    
                 </Container>
         </Box>
