f8539292cbe854817b8c9d022e1635de3c424cf9
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-03 09:16:29 +0530

    updated create bot

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,7 +1,41 @@
-export default function DashboardPage(){
-    return(
-        <div>
-            <h1>dashboard page</h1>
-        </div>
-    )
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+
+export default function DashboardPage() {
+  return (
+    <Box
+      sx={{
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box>
+        <Typography variant="h2">
+          Create Bot
+        </Typography>
+        <Box
+            sx={{
+                pt:7,
+                display:"flex",
+                flexDirection:"column"
+            }}
+            gap={3}
+        >
+            <TextField
+                label="Bot Name"
+                fullWidth
+            />
+
+            <Button 
+                variant="contained"
+                color="secondary"
+            > 
+                Create
+            </Button>
+
+        </Box>
+        
+      </Box>
+    </Box>
+  );
 }
