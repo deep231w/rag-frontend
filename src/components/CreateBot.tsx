63ed5ee45a3c7a39812ef15b5c05e89d7b888ca5
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-03 09:30:38 +0530

    bot creation func added

--- src/components/CreateBot.tsx
+++ src/components/CreateBot.tsx
@@ -1,6 +1,32 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 
 export default function DashboardPage() {
+  const admin= JSON.parse(localStorage.getItem("admin") || "null");
+
+  const [botName , setBotName]=useState<string>("");
+
+  async function createButton() {
+    console.log("admin is = ", admin)
+
+    if(!botName || !admin){
+      console.log("error , missing creds");
+      return;
+    }
+    try{
+      const res =await axios.post(`${import.meta.env.VITE_API_URL}/bot/create`,{
+        name:botName,
+        adminId:admin.userId
+      })
+
+      console.log("bot create response- ", res);
+
+    }catch(e){
+      console.log("error in create bot= ", e);
+    }
+    
+  }
   return (
     <Box
       sx={{
@@ -22,6 +48,7 @@ export default function DashboardPage() {
             gap={3}
         >
             <TextField
+                onChange={(e)=>setBotName(e.target.value)}
                 label="Bot Name"
                 fullWidth
             />
@@ -29,6 +56,7 @@ export default function DashboardPage() {
             <Button 
                 variant="contained"
                 color="secondary"
+                onClick={createButton}
             > 
                 Create
             </Button>
