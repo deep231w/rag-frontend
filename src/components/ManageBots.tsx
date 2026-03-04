fc49676d5bfe90b1d78a2738c2fdc9852f2c5032
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-04 08:14:10 +0530

    updated

--- src/components/ManageBots.tsx
+++ src/components/ManageBots.tsx
@@ -1,10 +1,14 @@
-import { Alert, Box, Grid, Snackbar, Typography, type AlertColor } from "@mui/material";
+import { Alert, Box, Grid, Paper, Snackbar, Typography, type AlertColor } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function ManageBots(){
 
     const admin= JSON.parse(localStorage.getItem("admin") || "null");
+
+    const [bots , setBots]=useState<
+        { id: string; adminId: string; botName: string }[]
+    >([]);
 
       const [alert , setAlert]=useState<{
         message:string,
@@ -25,10 +29,20 @@ export default function ManageBots(){
 
         try{
             const response= await axios.get(`${import.meta.env.VITE_API_URL}/bot/getbotsdata`,{
-                params:{adminId:admin.userId}
+                params:{
+                    adminId:admin.userId
+                }
             })
 
-            console.log("response of bots - ", response);
+            console.log("response of bots - ", response.data);
+            const botsdata=response.data.bots.map((bot:any)=>({
+                id:bot._id,
+                adminId:bot.adminId,
+                botName:bot.name
+            })); 
+            setBots(botsdata);
+
+            console.log("raw bots= ", botsdata)
 
         }catch(e){
             console.log("error in fetch bots -", e);
@@ -38,7 +52,7 @@ export default function ManageBots(){
 
     useEffect(()=>{
         fetchBots();
-    },[admin])
+    },[admin?.userId])
 
     function handleClose (){
         // setSuccess(false);
@@ -60,12 +74,29 @@ export default function ManageBots(){
             >
                 <Typography variant="h4">Manage Your Bots</Typography>
             </Box>
-            <Box>
-                <Grid>
-
-                    <Typography variant="h6">Bot1</Typography>
-                    <Typography variant="h6">Bot2</Typography>
-                    <Typography variant="h6">Bot3</Typography>
+            <Box sx={{mt:4}}>
+                <Grid container spacing={3}>
+                    {bots.length>=1 ? (bots.map((bot)=>(
+                        <Grid key={bot.id} size={{xs:12 , sm:6 , md:4}} >
+                            <Paper sx={{p:3}}>
+                                <Typography
+                                    variant="h6"
+                                >
+                                    {bot.botName}
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                    ))):(
+                        <Box
+                            sx={{
+                                display:"flex",
+                                alignItems:"center",
+                                justifyContent:"center"
+                            }}
+                        >
+                            <Typography variant="h5">No Bot Available</Typography>
+                        </Box>
+                    )}
                 </Grid>
             </Box>
             {alert.open && 
