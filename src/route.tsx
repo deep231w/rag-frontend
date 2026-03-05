9da6f5c97fbb80deaacb11b3d284e9fc05d15a83
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-05 16:30:24 +0530

    route protection added

--- src/route.tsx
+++ src/route.tsx
@@ -1,15 +1,35 @@
-import { BrowserRouter, Routes ,Route} from "react-router-dom";
+import { BrowserRouter, Routes ,Route, Navigate} from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 
+const ProtectedRoute:React.FC<{children:React.ReactElement}>=({children})=>{
+    const token= localStorage.getItem("token");
+
+    if(!token){
+        return <Navigate to={"/signin"}/>
+    }
+
+    return children;
+}
+
 export  default function AppRoutes(){
+    const token= localStorage.getItem("token");
+
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/signin" element={<SignIn/>}/>
-                <Route path="/home/*" element={<Home/>}/>
+                <Route path="/"
+                        element={token ? <Navigate to={"/home"}/>: <Navigate to={"/signin"}/>}
+                />
+                <Route path="/signup" element={token ? <Navigate to={"/home"}/>:<SignUp/>}/>
+                <Route path="/signin" element={token ? <Navigate to={"/home"}/>:<SignIn/>}/>
+                
+                    <Route path="/home/*" element={
+                        <ProtectedRoute>
+                            <Home/>
+                        </ProtectedRoute>
+                        }/>
             </Routes>
         </BrowserRouter>
     )
