27474daf82f64cb7630ac97266ca4d247e003ca8
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-03-08 20:53:27 +0530

    upated drawer

--- src/components/CustomDrawer.tsx
+++ src/components/CustomDrawer.tsx
@@ -7,9 +7,10 @@ export default function CustomDrawer({bot ,onClose}:{bot:any , onClose:()=>void}
     return(
         <Drawer anchor="right" open onClose={onClose}>
             <Box
-                sx={{width:700 , p:3}}
+                sx={{width:700 , p:3, mt:5 , display:"flex" , flexDirection:"column"  ,alignItems:"center"}}
             >
-                <Typography> hello d</Typography>
+                <Typography variant="h4">{bot?.botName}</Typography>
+                
             </Box>
         </Drawer>
     )
